24b854d264f6ce29f7602749bb86b9b677f0dc05
Mephostopilis <mephostopilis@gmail.com>
2018-09-03 00:04:48 +0800

    1

---
+++ src/pages/index.jsx
@@ -0,0 +1,22 @@
+// import { Rate } from 'antd';
+const App = props => {
+  const Head = () => {
+    return (
+      <div id="head">
+        <div class="layout">
+          <div class="logo" />
+          <img src="http://img.pipacoding.com/assets/pc/official/large/logo2.png" alt="这是一个logo" />
+        </div>
+        <div class="children animated fadeInUp">
+          <img src="http://img.pipacoding.com/assets/pc/official/icon/key_1.png" />
+          <img src="http://img.pipacoding.com/assets/pc/official/icon/key_2.png" />
+          <img src="http://img.pipacoding.com/assets/pc/official/icon/key_3.png" />
+          <img src="http://img.pipacoding.com/assets/pc/official/icon/key_4.png" />
+        </div>
+      </div>);
+  };
+
+  return (<div>{ Head() }</div>);
+}
+
+export default App;
